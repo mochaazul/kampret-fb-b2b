8dc4487e440c5863c24033d929c4af4955b25c6e
andi_RW <nenin.andilono@rebelworks.co>
2022-11-15 18:12:47 +0700

    [slicing] delivery report issue : - add translation

---
+++ src/translations/resources/en.ts
@@ -0,0 +1,12 @@
+export default {
+	welcome: 'Welcome to Rebelworks Skeleton',
+	actions: {
+		continue: 'Continue',
+	},
+	example: {
+		helloUser: 'I am a fake user, my name is {{name}}',
+		labels: {
+			userId: 'Enter a user id',
+		},
+	},
+};
